b20db3499b90d75dcaa2af8c908f1f658c946141
Carlos Vega <cvega2@internal.mcmcg.com>
2016-02-26 16:20:27 -0600

    Enabling CORS

--- app.js
+++ app.js
@@ -2,16 +2,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var repositories = require('./routes/repositories');
-var stackoverflow = require('./routes/stackoverflow'); 
+var stackoverflow = require('./routes/stackoverflow');
 var app = express();
 
 
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', repositories); //This is our route middleware
-app.use('/api', stackoverflow); 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.use('/api', repositories); //This is our route middleware
+app.use('/api', stackoverflow);
 
 app.set('port', process.env.PORT || 8000);
 
